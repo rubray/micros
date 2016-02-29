576ee444bb52a1be4e10f54959aea938a327a20e
emanuel estrella <bryanrengif@gmail.com>
2016-02-29 15:50:31 -0500

    mi aMOR  RUBY

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>microsf</title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+<center>
+<img src="1.jpg" width="2000" alt="">
+</center>
+<a target="_blank" href="https://www.google.com.pe">IR A GOOGLE</a>
+<marquee>
+<h1>bienvenido al mundo heroku </h1>
+</marquee>
+	
+</body>
+</html>
